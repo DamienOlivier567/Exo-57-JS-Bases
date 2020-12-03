367475300d2b958582913cd9eb08c43fd8e53ee7
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2020-12-03 12:33:03 +0100

    Exo-57

---
+++ exo 5/app.js
@@ -0,0 +1,9 @@
+let nb1 = prompt("Entrez un nombre decimal");
+
+let nb2 = prompt('Idem')
+
+alert(nb1 * nb2)
+
+nb1.log(Math.trunc);
+
+
---
+++ exo 6/app.js
@@ -0,0 +1,57 @@
+let maVar = prompt("Entrez votre âge");
+
+switch(maVar) {
+    case '1':
+        alert("Serieux !!");
+        break;
+    case '2':
+        alert("Serieux !!");
+        break;
+    case '3':
+        alert("Serieux !!");
+        break;
+    case '4':
+        alert("Serieux !!");
+        break;
+    case '5':
+        alert("Serieux !!");
+        break;
+    case '6':
+        alert("Serieux !!");
+        break;
+    case '7':
+        alert("Serieux !!");
+        break;
+    case '8':
+        alert("Serieux !!");
+        break;
+    case '9':
+        alert("Serieux !!");
+        break;
+    case '10':
+        alert("Vous êtes mineur");
+        break;
+    case '11':
+        alert("Vous êtes mineur");
+        break;
+    case '12':
+        alert("Vous êtes mineur");
+        break;
+    case '13':
+        alert("Vous êtes mineur");
+        break;
+    case '14':
+        alert("Vous êtes mineur");
+        break;
+    case '15':
+        alert("Vous êtes mineur");
+        break;
+    case '16':
+        alert("Vous êtes mineur");
+        break;
+    case '17':
+        alert("Vous êtes mineur!");
+        break;
+    default:
+        alert("Vous êtes majeur");
+}
